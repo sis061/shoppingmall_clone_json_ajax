5abd809d6242c5b35018d974e8ce23bea43573db
sis061 <sis061@naver.com>
2023-09-13 11:54:06 +0900

    first commit

--- js/main02.js
+++ js/main02.js
@@ -5,9 +5,9 @@ $(function(){
         success: function(data){
             if(data.length>0){
                 for(var i in data){
-                    $(".timesale_product").eq(i).append("<a href='#'>" + "<img src='img/" + data[i].url +"alt='그라인더'></a>");
+                    $(".timesale_product").eq(i).append("<a href='#'>" + "<img src='img/" + data[i].url +"' alt='그라인더'></a>");
                     $(".timesale_product").eq(i).append("<a href='#'>" + "<h3>" + data[i].title +"</h3></a>");
-                    $(".timesale_product").eq(i).append("<a href='#'>" + "<span id='timesale_0"+ [i] + "'>" + data[i].price +"</span></a>");
+                    $(".timesale_product").eq(i).append("<a href='#'>" + "<span id='timesale_0"+ i + "'>" + data[i].price +"</span></a>");
                     $(".timesale_product").eq(i).append("<a href='#'>" + "<p>" + data[i].originPrice +"</p></a>");
                 }
             }
